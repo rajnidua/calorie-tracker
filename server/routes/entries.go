1ada9ad35b32f2aa7542eabd13e6c93b93a91f71
Rajni Dua <rajni.dua14@gmail.com>
2022-07-14 05:33:07 +0930

    update ingredients and update entry functions

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -10,7 +10,31 @@ import("context"
 var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
 
 func AddEntry(c *gin.Context){
+var ctx,cancel = context.WithTimeout(context.Background(),100*time.second)
+var entry models.Entry
 
+
+if err : = c.BindJSON(&entry); err!=nil{
+	c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+	fmt.println(err)
+	return
+}
+validationErr:=validate.Struct(entry)
+if validationErr !=nil{
+	c.JSON(http.StatusInternalServerError,gin.H{"error":validationErr.Error()})
+	fmt.Println(validationErr)
+	return
+}
+entry.ID =primitive.NewObjectID()
+result,insertErr := entryCollection.InsertOne(ctx,entry)
+if insertErr != nil{
+	msg := fmt.Sprintf("order item was not created")
+	c.JSON(http.StatusInternalServerError,gin.H{"error":msg})
+	fmt.Println(insertErr)
+	return
+}
+defer cancel()
+c.JSON(http.StatusOK,result)
 }
 
 func GetEnteries(c *gin.Context){
@@ -51,10 +75,54 @@ fmt.Println(entry)
 c.JSON(http.StatusOK,entry)
 }
 func UpdateIngredient(c *gin.Context){
-
+entryID:= c.Params.ByName("id")
+	docID, _ := primitve.ObjectIDFromHex(entryID)
+	var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+	 
+	type Ingredient struct {
+		Ingredients *string `json:"ingredients"`
+	}
+var ingredient Ingredient
+	if err : = c.BindJSON(&entry); err!=nil{
+	c.JSON(http.StatusInternalServerError,gin.H{"error":validationErr.Error()})
+	fmt.println(validationErr)
+	return
+}
+result,err:=entryCollection.UpdateOne(ctx,bson.M{"_id":docID}),
+bson.D{"$set",bson.D{{"ingredients",ingredient.Ingredients}}}},
+if err !=nil{
+	c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+	fmt.Println(err)
+	return
+}
+defer cancel()
+c.JSON(http.StatusOK,result.ModifiedCount)
 }
 
-func UpdateEntry(c *gin.Context){}
+func UpdateEntry(c *gin.Context){
+	entryID:= c.Params.ByName("id")
+	docID, _ := primitve.ObjectIDFromHex(entryID)
+	var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+	var entry model.Entry
+
+	if err : = c.BindJSON(&entry); err!=nil{
+	c.JSON(http.StatusInternalServerError,gin.H{"error":validationErr.Error()})
+	fmt.println(validationErr)
+	return
+}
+result,err := entryCollection.ReplaceOne(ctx,bson.M{"_id":docID},bson.M{"dish":entry.Dish,
+"fat":entry.Fat,
+"ingredients":entry.Ingredients,
+"calories":entry.Calories},)
+if err != nil{
+	c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+	fmt.Println(err)
+	return
+}
+defer cancel()
+c.JSON(http.StatusOK,result.ModifiedCount)
+
+}
 
 func DeleteEntry(c *gin.Context){
 entryID := c.Params.ByName("id")
