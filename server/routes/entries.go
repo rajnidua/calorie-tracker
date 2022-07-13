5ac2033963c6c628c61d47db15a52afc83ac14f1
Rajni Dua <rajni.dua14@gmail.com>
2022-07-14 04:53:37 +0930

    added connection and Get by id method

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -1,7 +1,13 @@
 package routes
 
-import("")
-var entryCollection *mongo.Collection = openCollection(Client, "calories")
+import("context"
+"fmt"
+"time"
+"github.com/gin-gonic/gin"
+"go.mongodb.org/mongo-driver/bson/primitive"
+"go.mongodb.org/mongo-driver/mongo"
+"gopkg.in/mgo.v2/bson")
+var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
 
 func AddEntry(c *gin.Context){
 
@@ -33,7 +39,16 @@ func GetEnteriesByIngredient(c *gin.Context){
 }
 
 func GetEntryById(c *gin.Context){
-
+EntryID :=c.Params.ByName("id")
+docID, _ :=primitive.ObjectIDFromHex(EntryID)
+var ctx,cancel = context.WithTimeout(context.Background(),100*time.Second)
+var entry bson.M
+if err := entryCollection.FindOne(ctx,bson.M{"_id":docID}).Decode(&entry); err !=nil{c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+fmt.Println(err)
+return}
+defer canceel()
+fmt.Println(entry)
+c.JSON(http.StatusOK,entry)
 }
 func UpdateIngredient(c *gin.Context){
 
